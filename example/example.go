04154e5d6f3fbeda6fb1b8f3331ace470958133b
Utkan Güngördü <utkan@freeconsole.org>
2011-08-18 11:37:47 +0900

    gofmt sources

--- example/example.go
+++ example/example.go
@@ -13,17 +13,23 @@ var filename = flag.String("f", "example.7z", "Filename")
 func main() {
 	flag.Parse()
 	z, err := sz.Open(*filename)
-	if err != nil { log.Fatal(err) }
-	
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	for _, f := range z.File {
- 		fmt.Println("Extracting ", f.Name)
+		fmt.Println("Extracting ", f.Name)
 		r, err := f.Open()
-		if err != nil { log.Fatal(err) }
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		//file := f.ReadAll()
 		file := make([]byte, f.Size)
 		_, err = r.Read(file)
-		if err != nil { log.Fatal(err) }
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		ioutil.WriteFile(f.Name, f.Dump(), 0666)
 	}
--- sevenzip.go
+++ sevenzip.go
@@ -101,45 +101,44 @@ import (
 
 type SevenZip struct {
 	allocImp, allocTempImp C.ISzAlloc
-	archiveStream C.CFileInStream
-	lookStream C.CLookToRead
-	db C.CSzArEx
-	
-	blockIndex C.UInt32
+	archiveStream          C.CFileInStream
+	lookStream             C.CLookToRead
+	db                     C.CSzArEx
+
+	blockIndex    C.UInt32
 	outBufferSize C.size_t
-	outBuffer *C.Byte
-	
+	outBuffer     *C.Byte
+
 	l sync.Mutex
-	
+
 	File []*File
 }
 
 type NtfsFileTime struct {
-  Low uint32
-  High uint32
+	Low  uint32
+	High uint32
 }
 
 type FileHeader struct {
-  MTime NtfsFileTime
-  Size uint64
-  Crc uint32
-  Attrib uint32
-  HasStream byte
-  IsDir byte
-  IsAnti byte
-  CrcDefined byte
-  MTimeDefined byte
-  AttribDefined byte
+	MTime         NtfsFileTime
+	Size          uint64
+	Crc           uint32
+	Attrib        uint32
+	HasStream     byte
+	IsDir         byte
+	IsAnti        byte
+	CrcDefined    byte
+	MTimeDefined  byte
+	AttribDefined byte
 }
 
 type File struct {
 	*FileHeader
-	Name string
+	Name  string
 	Index int
-	z *SevenZip
-	buf []byte
+	z     *SevenZip
+	buf   []byte
 }
-
 
 // Returns the buffered contents of an opened file.
 // This function won't make "unnecessary" copies of the underlying data.
@@ -150,7 +149,9 @@ func (f *File) ReadAll() []byte {
 // This function will call Extract.
 func (f *File) Open() (*bytes.Buffer, os.Error) {
 	buf, err := f.z.Extract(f.Index)
-	if err != nil { return nil, err }
+	if err != nil {
+		return nil, err
+	}
 	f.buf = buf
 	return bytes.NewBuffer(buf), err
 }
@@ -158,32 +159,38 @@ func (f *File) Open() (*bytes.Buffer, os.Error) {
 // This function will call ExtractUnsafe.
 func (f *File) OpenUnsafe() (*bytes.Buffer, os.Error) {
 	buf, err := f.z.ExtractUnsafe(f.Index)
-	if err != nil { return nil, err }
+	if err != nil {
+		return nil, err
+	}
 	f.buf = buf
 	return bytes.NewBuffer(buf), err
 }
 
 func init() {
-	C.CrcGenerateTable();
+	C.CrcGenerateTable()
 }
 
 func (z *SevenZip) name(i int) string {
-	length := C.SzArEx_GetFileNameUtf16(&z.db, C.size_t(i), (*C.UInt16)(C._null));
+	length := C.SzArEx_GetFileNameUtf16(&z.db, C.size_t(i), (*C.UInt16)(C._null))
 	temp := C.SzAlloc(C._null, length*2)
-	if temp == C._null { panic("sevenzip: out of memory") } //FIXME: don't panic!
+	if temp == C._null {
+		panic("sevenzip: out of memory")
+	} //FIXME: don't panic!
 	defer C.SzFree(C._null, temp)
 
-	C.SzArEx_GetFileNameUtf16(&z.db, C.size_t(i), (*C.UInt16)(temp));
+	C.SzArEx_GetFileNameUtf16(&z.db, C.size_t(i), (*C.UInt16)(temp))
 
 	var destLen C.size_t
 	C.Utf16_To_Utf8((*C.Byte)(C._null), &destLen, (*C.UInt16)(temp), length)
 	destLen++
 	temp2 := C.SzAlloc(C._null, destLen)
-	if temp2 == C._null { panic("sevenzip: out of memory") } //FIXME: don't panic!
+	if temp2 == C._null {
+		panic("sevenzip: out of memory")
+	} //FIXME: don't panic!
 	defer C.SzFree(C._null, temp2)
-	
+
 	C.Utf16_To_Utf8((*C.Byte)(temp2), &destLen, (*C.UInt16)(temp), length)
-	
+
 	name := C.GoString((*C.char)(temp2))
 	return name
 }
@@ -207,32 +214,32 @@ func (z *SevenZip) Close() {
 func Open(filename string) (*SevenZip, os.Error) {
 	z := new(SevenZip)
 	z.allocImp.Alloc = (*[0]uint8)(C._szAlloc)
- 	z.allocImp.Free = (*[0]uint8)(C._szFree)
- 	z.allocTempImp.Alloc = (*[0]uint8)(C._szAllocTemp)
- 	z.allocTempImp.Free = (*[0]uint8)(C._szFreeTemp)
-	
+	z.allocImp.Free = (*[0]uint8)(C._szFree)
+	z.allocTempImp.Alloc = (*[0]uint8)(C._szAllocTemp)
+	z.allocTempImp.Free = (*[0]uint8)(C._szFreeTemp)
+
 	cfilename := C.CString(filename)
 	defer C.free(unsafe.Pointer(cfilename))
 	if C.InFile_Open(&z.archiveStream.file, cfilename) != 0 {
 		return nil, os.NewError("sevenzip: file not found: " + filename)
 	}
 
-  C.FileInStream_CreateVTable(&z.archiveStream);
-  C.LookToRead_CreateVTable(&z.lookStream, 0); //False??
-  
-  z.lookStream.realStream = &z.archiveStream.s;
-  C.LookToRead_Init(&z.lookStream);
+	C.FileInStream_CreateVTable(&z.archiveStream)
+	C.LookToRead_CreateVTable(&z.lookStream, 0) //False??
 
-//   C.CrcGenerateTable();  
+	z.lookStream.realStream = &z.archiveStream.s
+	C.LookToRead_Init(&z.lookStream)
 
-	C.SzArEx_Init(&z.db);
+	//   C.CrcGenerateTable();  
+
+	C.SzArEx_Init(&z.db)
 	if C.SzArEx_Open(&z.db, &z.lookStream.s, &z.allocImp, &z.allocTempImp) != C._SZ_OK {
 		return nil, os.NewError("sevenzip: failed to open archive: " + filename)
 	}
 
 	z.File = make([]*File, int(z.db.db.NumFiles))
-	
-	for i:=0; i<int(z.db.db.NumFiles); i++ {
+
+	for i := 0; i < int(z.db.db.NumFiles); i++ {
 		cf := C._CSzFileItemArrayIndex(z.db.db.Files, C.size_t(i))
 		header := (*FileHeader)(unsafe.Pointer(cf))
 		name := z.name(i)
@@ -258,13 +265,13 @@ func (z *SevenZip) ExtractUnsafe(i int) ([]byte, os.Error) {
 		&z.allocImp, &z.allocTempImp) != C._SZ_OK {
 		return []byte{}, os.NewError("extract failed")
 	}
-	
+
 	cbuf := C._ByteArrayIndex(z.outBuffer, offset)
 	clen := int(outSizeProcessed)
 
-	file := (*[1 << 30]byte)(unsafe.Pointer(cbuf))[:clen] 
-	(*reflect.SliceHeader)(unsafe.Pointer(&cbuf)).Cap = clen 
-	
+	file := (*[1 << 30]byte)(unsafe.Pointer(cbuf))[:clen]
+	(*reflect.SliceHeader)(unsafe.Pointer(&cbuf)).Cap = clen
+
 	return file, nil
 }
 
@@ -274,12 +281,14 @@ func (z *SevenZip) ExtractUnsafe(i int) ([]byte, os.Error) {
 // buffer is flushed.
 // The catch is, this function, during it's execution,
 // will use up double of the size of the ith file.
-func (z *SevenZip) Extract(i int) ([]byte,os.Error) {
+func (z *SevenZip) Extract(i int) ([]byte, os.Error) {
 	z.l.Lock()
 	defer z.l.Unlock()
 
 	cfile, err := z.ExtractUnsafe(i)
-	if err != nil { return []byte{}, err }
+	if err != nil {
+		return []byte{}, err
+	}
 	file := make([]byte, len(cfile))
 	copy(file, cfile)
 	z.flushBuffer()
